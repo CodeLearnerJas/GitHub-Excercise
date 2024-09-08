3dc82afb49c5d2f8593136c9712cc34eb8a0653f
CodeLearnerJas <yangtianjin2013@gmai.com>
2024-09-08 15:51:38 +1000

    Initial Commit

---
+++ GitHub Excercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub Excercise
+//
+//  Created by GuitarLearnerJas on 8/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub Excercise/GitHub_ExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExcerciseApp.swift
+//  GitHub Excercise
+//
+//  Created by GuitarLearnerJas on 8/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
